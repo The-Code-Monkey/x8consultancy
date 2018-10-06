dc0df6c79a693d29e8a652b3577c9f7a3116d432
Andrew Wilson <andy.wilson1234@gmail.com>
2018-10-06 16:18:24 +0100

    Updates

---
+++ precache-manifest.ab556b4e20d723cbd49e06cfb07ea4ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4c4048fa16b061a7c77",
+    "url": "/x8consultancy/static/js/runtime~main.b4c4048f.js"
+  },
+  {
+    "revision": "6fe67170b6364ba71b66",
+    "url": "/x8consultancy/static/js/main.6fe67170.chunk.js"
+  },
+  {
+    "revision": "71f6f3e8c604ca82dbf8",
+    "url": "/x8consultancy/static/js/1.71f6f3e8.chunk.js"
+  },
+  {
+    "revision": "6fe67170b6364ba71b66",
+    "url": "/x8consultancy/static/css/main.4a8a0c2a.chunk.css"
+  },
+  {
+    "revision": "71f6f3e8c604ca82dbf8",
+    "url": "/x8consultancy/static/css/1.66f3835f.chunk.css"
+  },
+  {
+    "revision": "32addb71bb225c945beb384e8563d3ac",
+    "url": "/x8consultancy/index.html"
+  }
+];
